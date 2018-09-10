8acab50bc255f5ffd7a429358cab77fe1c6769d5
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-09-10 15:37:11 +0200

    Discovery server

--- discovery/src/main/java/pl/training/cloud/discovery/DiscoveryServer.java
+++ discovery/src/main/java/pl/training/cloud/discovery/DiscoveryServer.java
@@ -1,5 +1,6 @@
 package pl.training.cloud.discovery;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -8,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServer {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(DiscoveryServer.class, args);
     }
 
 }
