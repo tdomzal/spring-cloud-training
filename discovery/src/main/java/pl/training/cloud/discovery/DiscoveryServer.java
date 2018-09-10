83e7cc5d3e3ea61b857a8b190c49622f861032ac
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-09-10 14:58:19 +0200

    Configuration server

---
+++ discovery/src/main/java/pl/training/cloud/discovery/DiscoveryServer.java
@@ -0,0 +1,12 @@
+package pl.training.cloud.discovery;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
