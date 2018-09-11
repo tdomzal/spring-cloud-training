4703c752d5fbda397d23e87afced63f9642361de
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-09-11 16:48:43 +0200

    Communication

--- run-kafka.sh
+++
@@ -1,3 +0,0 @@
-#https://medium.com/@shaaslam/installing-apache-kafka-on-windows-495f6f2fd3c8
-#!/bin/sh
-docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=localhost --env ADVERTISED_PORT=9092 spotify/kafka
