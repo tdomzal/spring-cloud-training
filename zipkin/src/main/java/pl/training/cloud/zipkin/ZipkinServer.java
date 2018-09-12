cd67751053d1ca4b86ae6ca898fdab318cfe0eb7
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-09-12 11:31:00 +0200

    Availability

---
+++ zipkin/src/main/java/pl/training/cloud/zipkin/ZipkinServer.java
@@ -0,0 +1,15 @@
+package pl.training.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServer.class, args);
+    }
+
+}
