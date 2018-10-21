d19adfecc2aae4a81ea40051d1eb717fe3f10335
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-10-21 10:50:33 +0200

    test

--- departments/src/main/java/pl/training/cloud/departments/repository/DepartmentsRepository.java
+++ departments/src/main/java/pl/training/cloud/departments/repository/DepartmentsRepository.java
@@ -1,12 +1,18 @@
 package pl.training.cloud.departments.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.training.cloud.departments.model.Department;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentsRepository extends JpaRepository<Department, Long> {
 
     Optional<Department> getById(Long id);
+
+    @Query("select d from Department d where d.name = :department_name")
+    List<Department> getDepartments(@Param("department_name") String department);
 
 }
