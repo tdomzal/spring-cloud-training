56a454262bddc60db730802b4ebf997797c570be
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-10-21 11:56:10 +0200

    test

--- users/src/main/java/pl/training/cloud/users/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.training.cloud.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.training.cloud.users.model.User;
-
-public interface UsersRepository extends JpaRepository<User, Long> {
-}
